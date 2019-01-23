129f774829ac21b159096e07f345b6bb9c98e0ce
Yi Xie <jasper@Yis-MacBook-Pro.local>
2019-01-22 22:28:53 -0500

    added TreeNode and LC 596

---
+++ src/com/jasper/dfs/LC596_MinimumSubtree.java
@@ -0,0 +1,28 @@
+package com.jasper.dfs;
+
+public class LC596_MinimumSubtree {
+
+	TreeNode subtree = null;
+	int min = Integer.MAX_VALUE;
+
+	public TreeNode findSubtree(TreeNode root) {
+		helper(root);
+		return subtree;
+	}
+
+	// 分治法通常会把返回统计的东西返回
+	private int helper(TreeNode node) {
+		if (node == null)
+			return 0;
+
+		int sum = node.val + helper(node.left) + helper(node.right);
+
+		if (sum < min) {
+			min = sum;
+			subtree = node;
+		}
+
+		return sum;
+	}
+
+}
