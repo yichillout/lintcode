a785b6940f11b22c8372b2be341f3ddf93692f92
Yi Xie <jasper@Yis-MacBook-Pro.local>
2019-01-23 22:32:35 -0500

    added LC246

---
+++ src/com/jasper/dfs/LC246_BinaryTreePathSumII.java
@@ -0,0 +1,42 @@
+package com.jasper.dfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC246_BinaryTreePathSumII {
+
+	public List<List<Integer>> binaryTreePathSum2(TreeNode root, int target) {
+		// Write your code here
+		List<List<Integer>> results = new ArrayList<List<Integer>>();
+		ArrayList<Integer> buffer = new ArrayList<Integer>();
+		if (root == null)
+			return results;
+		findSum(root, target, buffer, 0, results);
+		return results;
+	}
+
+	public void findSum(TreeNode head, int sum, ArrayList<Integer> buffer, int level, List<List<Integer>> results) {
+		
+		if (head == null)
+			return;
+		
+		int tmp = sum;
+		
+		buffer.add(head.val);
+		
+		for (int i = level; i >= 0; i--) {
+			tmp -= buffer.get(i);
+			if (tmp == 0) {
+				List<Integer> temp = new ArrayList<Integer>();
+				for (int j = i; j <= level; ++j)
+					temp.add(buffer.get(j));
+				results.add(temp);
+			}
+		}
+		
+		findSum(head.left, sum, buffer, level + 1, results);
+		findSum(head.right, sum, buffer, level + 1, results);
+		
+		buffer.remove(buffer.size() - 1);
+	}
+}
