3ca6e16add43c1fd3d0e3bc1471dd2806f302358
Yi Xie <jasper@Yis-MacBook-Pro.local>
2019-01-23 23:37:07 -0500

    added comment in LC88

--- src/com/jasper/dfs/LC88_LowestCommonAncestorOfBinaryTree.java
+++ src/com/jasper/dfs/LC88_LowestCommonAncestorOfBinaryTree.java
@@ -10,6 +10,12 @@ public class LC88_LowestCommonAncestorOfBinaryTree {
 	 * 
 	 * @return: Return the least common ancestor(LCA) of the two nodes.
 	 */
+	
+	// 在root为根的二叉树中找A,B的LCA:
+    // 如果找到了就返回这个LCA
+    // 如果只碰到A，就返回A
+    // 如果只碰到B，就返回B
+    // 如果都没有，就返回null
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode node1, TreeNode node2) {
         if(root == null || root == node1 || root == node2)
             return root;
