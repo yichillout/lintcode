7aa24b58f4ef475d303a02b4ca7b480b40987163
Yi Xie <jasper@Yis-MacBook-Pro.local>
2019-01-24 23:59:23 -0500

    added LC11

---
+++ src/com/jasper/dfs/LC11_SearchRangeInBinarySearchTree.java
@@ -0,0 +1,31 @@
+package com.jasper.dfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC11_SearchRangeInBinarySearchTree {
+
+	public List<Integer> searchRange(TreeNode root, int k1, int k2) {
+		List<Integer> result = new ArrayList<Integer>();
+		helper(root, result, k1, k2);
+		return result;
+	}
+
+	private void helper(TreeNode node, List<Integer> result, int k1, int k2) {
+		if (node == null)
+			return;
+
+		if (node.val > k1) {
+			helper(node.left, result, k1, k2);
+		}
+
+		if (node.val >= k1 && node.val <= k2) {
+			result.add(node.val);
+		}
+
+		if (node.val < k2) {
+			helper(node.right, result, k1, k2);
+		}
+
+	}
+}
