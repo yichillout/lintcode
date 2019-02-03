244283fcb4ee8aec609852334628cd24b73d4979
Yi Xie <jasper@Yis-MacBook-Pro.home>
2019-02-03 15:50:58 -0500

    added LC81

---
+++ src/com/jasper/priorityqueue/LC81_FindMedianFromDataStream.java
@@ -0,0 +1,51 @@
+package com.jasper.priorityqueue;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class LC81_FindMedianFromDataStream {
+
+	private PriorityQueue<Integer> maxHeap, minHeap;
+	private int numOfElements = 0;
+
+	public int[] medianII(int[] nums) {
+		// write your code here
+		Comparator<Integer> revCmp = new Comparator<Integer>() {
+			@Override
+			public int compare(Integer left, Integer right) {
+				return right.compareTo(left);
+			}
+		};
+		int cnt = nums.length;
+		maxHeap = new PriorityQueue<Integer>(cnt, revCmp);
+		minHeap = new PriorityQueue<Integer>(cnt);
+		int[] ans = new int[cnt];
+		for (int i = 0; i < cnt; ++i) {
+			addNumber(nums[i]);
+			ans[i] = getMedian();
+		}
+		return ans;
+	}
+
+	void addNumber(int value) {
+		maxHeap.add(value);
+		if (numOfElements % 2 == 0) {
+			if (minHeap.isEmpty()) {
+				numOfElements++;
+				return;
+			} else if (maxHeap.peek() > minHeap.peek()) {
+				Integer maxHeapRoot = maxHeap.poll();
+				Integer minHeapRoot = minHeap.poll();
+				maxHeap.add(minHeapRoot);
+				minHeap.add(maxHeapRoot);
+			}
+		} else {
+			minHeap.add(maxHeap.poll());
+		}
+		numOfElements++;
+	}
+
+	int getMedian() {
+		return maxHeap.peek();
+	}
+}
