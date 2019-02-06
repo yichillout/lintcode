3de763ab5f32ed9dc30c6f015c8a885a878374a2
Yi Xie <jasper@Yis-MacBook-Pro.home>
2019-02-05 21:54:19 -0500

    modified LC453

--- src/com/jasper/dfs/LC453_FlattenBinaryTreeToLinkedList.java
+++ src/com/jasper/dfs/LC453_FlattenBinaryTreeToLinkedList.java
@@ -24,64 +24,64 @@ public class LC453_FlattenBinaryTreeToLinkedList {
 		flatten1(right);
 
 	}
-	
+
 	// version 2: Divide & Conquer
 	public void flatten2(TreeNode root) {
-        helper(root);
-    }
-    
-    // flatten root and return the last node
-    private TreeNode helper(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
-        
-        TreeNode leftLast = helper(root.left);
-        TreeNode rightLast = helper(root.right);
-        
-        // connect leftLast to root.right
-        if (leftLast != null) {
-            leftLast.right = root.right;
-            root.right = root.left;
-            root.left = null;
-        }
-        
-        if (rightLast != null) {
-            return rightLast;
-        }
-        
-        if (leftLast != null) {
-            return leftLast;
-        }
-        
-        return root;
-    }
-    
-    // version 3: Non-Recursion
-    public void flatten3(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-        
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        
-        while (!stack.empty()) {
-            TreeNode node = stack.pop();
-            if (node.right != null) {
-                stack.push(node.right);
-            }
-            if (node.left != null) {
-                stack.push(node.left);
-            }
-            
-            // connect 
-            node.left = null;
-            if (stack.empty()) {
-                node.right = null;
-            } else {
-                node.right = stack.peek();
-            }
-        }
-    }
+		helper(root);
+	}
+
+	// flatten root and return the last node
+	private TreeNode helper(TreeNode root) {
+		if (root == null) {
+			return null;
+		}
+
+		TreeNode leftLast = helper(root.left);
+		TreeNode rightLast = helper(root.right);
+
+		// connect leftLast to root.right
+		if (leftLast != null) {
+			leftLast.right = root.right;
+			root.right = root.left;
+			root.left = null;
+		}
+
+		if (rightLast != null) {
+			return rightLast;
+		}
+
+		if (leftLast != null) {
+			return leftLast;
+		}
+
+		return root;
+	}
+
+	// version 3: Non-Recursion
+	public void flatten3(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+
+		Stack<TreeNode> stack = new Stack<>();
+		stack.push(root);
+
+		while (!stack.empty()) {
+			TreeNode node = stack.pop();
+			if (node.right != null) {
+				stack.push(node.right);
+			}
+			if (node.left != null) {
+				stack.push(node.left);
+			}
+
+			// connect
+			node.left = null;
+			if (stack.empty()) {
+				node.right = null;
+			} else {
+				node.right = stack.peek();
+			}
+		}
+	}
 }
