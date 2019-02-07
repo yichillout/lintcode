ad20f6ba9caac75974983f8548532b7b10b5a259
Yi Xie <jasper@Yis-MacBook-Pro.home>
2019-02-06 23:55:38 -0500

    LC585

---
+++ src/com/jasper/binarysearch/LC585_MaximumNumberInMountainSequence.java
@@ -0,0 +1,22 @@
+package com.jasper.binarysearch;
+
+public class LC585_MaximumNumberInMountainSequence {
+
+	public int mountainSequence(int[] nums) {
+		if (nums.length == 1)
+			return nums[0];
+
+		int start = 0;
+		int end = nums.length - 1;
+		while (start + 1 < end) {
+			int mid = start + (end - start) / 2;
+			if (mid - 1 >= 0 && nums[mid - 1] < nums[mid]) {
+				start = mid;
+			} else {
+				end = mid;
+			}
+		}
+
+		return Math.max(nums[start], nums[end]);
+	}
+}
