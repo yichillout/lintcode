7e1d56c401766ce103d84df5c22f129964d1c293
Yi Xie <jasper@Yis-MacBook-Pro.home>
2019-02-07 23:11:09 -0500

    added LC615

---
+++ src/com/jasper/topologicalsort/LC615CourseSchedule.java
@@ -0,0 +1,46 @@
+package com.jasper.topologicalsort;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LC615CourseSchedule {
+
+	public boolean canFinish(int numCourses, int[][] prerequisites) {
+		// Write your code here
+		List[] edges = new ArrayList[numCourses];
+		int[] degree = new int[numCourses];
+
+		for (int i = 0; i < numCourses; i++)
+			edges[i] = new ArrayList<Integer>();
+
+		for (int i = 0; i < prerequisites.length; i++) {
+			degree[prerequisites[i][0]]++;
+			edges[prerequisites[i][1]].add(prerequisites[i][0]);
+		}
+
+		Queue queue = new LinkedList();
+		for (int i = 0; i < degree.length; i++) {
+			if (degree[i] == 0) {
+				queue.add(i);
+			}
+		}
+
+		int count = 0;
+		while (!queue.isEmpty()) {
+			int course = (int) queue.poll();
+			count++;
+			int n = edges[course].size();
+			for (int i = 0; i < n; i++) {
+				int pointer = (int) edges[course].get(i);
+				degree[pointer]--;
+				if (degree[pointer] == 0) {
+					queue.add(pointer);
+				}
+			}
+		}
+
+		return count == numCourses;
+	}
+}
