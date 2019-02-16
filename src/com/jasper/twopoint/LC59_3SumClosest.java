477a84e398d4fa505604c2d5ad4130c0ebeab457
Yi Xie <jasper@Yis-MacBook-Pro.home>
2019-02-15 22:13:06 -0500

    added LC59

---
+++ src/com/jasper/twopoint/LC59_3SumClosest.java
@@ -0,0 +1,33 @@
+package com.jasper.twopoint;
+
+import java.util.Arrays;
+
+public class LC59_3SumClosest {
+
+	public int threeSumClosest(int[] numbers, int target) {
+		if (numbers == null || numbers.length < 3) {
+			return -1;
+		}
+
+		Arrays.sort(numbers);
+
+		int bestSum = numbers[0] + numbers[1] + numbers[2];
+		for (int i = 0; i < numbers.length; i++) {
+			int start = i + 1, end = numbers.length - 1;
+			while (start < end) {
+				int sum = numbers[i] + numbers[start] + numbers[end];
+				if (Math.abs(target - sum) < Math.abs(target - bestSum)) {
+					bestSum = sum;
+				}
+				if (sum < target) {
+					start++;
+				} else {
+					end--;
+				}
+			}
+		}
+
+		return bestSum;
+	}
+
+}
